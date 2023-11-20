4e888e65ea12e9197b5656db37397baabdd761c0
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-11-20 19:47:06 +0400

    +=8 end
    +=8

--- lesson-5/src/App.jsx
+++ lesson-5/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
   async function siteData() {
     const response = await fetch("https://api.vendoo.ge/api/beta/catalog?url=technics%2Ftelefonebi%2Fmobiluri-telefonebi&sort=popular&sortDir=desc&page=1&limit=20&fbclid=IwAR0s6u7MCxcfz2467VbymrazjdTEuGJfq6Y0YF22avUzoehaUs7aM0D6BBY");
     const list = await response.json()
-    // console.log(list.products)
+    // console.log(list)
 
     const filteredProducts = list.products.filter((el) => el.final_price != el.original_price)
     setProducts(filteredProducts)
-    const image = [
+    const imageM = [
        {
         url: 'https://vendoo.ge/details/technics/telefonebi/mobiluri-telefonebi/apple-iphone-13-6gb-128gb-model-a2633-pink-mobiluri-telefoni-281512/?keyword=Apple%20iPhone%2013%206GB%20128GB%20Model%20A2633%20Pink%20%E1%83%9B%E1%83%9D%E1%83%91%E1%83%98%E1%83%9A%E1%83%A3%E1%83%A0%E1%83%98%20%E1%83%A2%E1%83%94%E1%83%9A%E1%83%94%E1%83%A4%E1%83%9D%E1%83%9C%E1%83%98',
         img: './src/image/img2 (1).jpg'
@@ -37,10 +37,25 @@ function App() {
        {
         url: 'https://vendoo.ge/details/technics/telefonebi/mobiluri-telefonebi/apple-iphone-13-4gb-128gb-midnight-mobiluri-telefoni-286277/?keyword=Apple%20iPhone%2013%204GB%20128GB%20Midnight%20%E1%83%9B%E1%83%9D%E1%83%91%E1%83%98%E1%83%9A%E1%83%A3%E1%83%A0%E1%83%98%20%E1%83%A2%E1%83%94%E1%83%9A%E1%83%94%E1%83%A4%E1%83%9D%E1%83%9C%E1%83%98',
         img: './src/image/img4.jpg'
+       },
+
+       {
+        url: 'https://vendoo.ge/details/technics/telefonebi/mobiluri-telefonebi/apple-iphone-13-4gb-128gb-midnight-mobiluri-telefoni-286277/?keyword=Apple%20iPhone%2013%204GB%20128GB%20Midnight%20%E1%83%9B%E1%83%9D%E1%83%91%E1%83%98%E1%83%9A%E1%83%A3%E1%83%A0%E1%83%98%20%E1%83%A2%E1%83%94%E1%83%9A%E1%83%94%E1%83%A4%E1%83%9D%E1%83%9C%E1%83%98',
+        img: './src/image/img4.jpg'
+       },
+
+       {
+        url: 'https://vendoo.ge/details/technics/telefonebi/mobiluri-telefonebi/apple-iphone-13-4gb-128gb-midnight-mobiluri-telefoni-286277/?keyword=Apple%20iPhone%2013%204GB%20128GB%20Midnight%20%E1%83%9B%E1%83%9D%E1%83%91%E1%83%98%E1%83%9A%E1%83%A3%E1%83%A0%E1%83%98%20%E1%83%A2%E1%83%94%E1%83%9A%E1%83%94%E1%83%A4%E1%83%9D%E1%83%9C%E1%83%98',
+        img: './src/image/img4.jpg'
+       },
+
+       {
+        url: 'https://vendoo.ge/details/technics/telefonebi/mobiluri-telefonebi/apple-iphone-13-4gb-128gb-midnight-mobiluri-telefoni-286277/?keyword=Apple%20iPhone%2013%204GB%20128GB%20Midnight%20%E1%83%9B%E1%83%9D%E1%83%91%E1%83%98%E1%83%9A%E1%83%A3%E1%83%A0%E1%83%98%20%E1%83%A2%E1%83%94%E1%83%9A%E1%83%94%E1%83%A4%E1%83%9D%E1%83%9C%E1%83%98',
+        img: './src/image/img4.jpg'
        }
     ];
 
-    const imageMasiv = image.filter((el)=> el.url)
+    const imageMasiv = imageM.filter((el)=> el.url)
 
     // console.log(imageMasiv)
     setImage(imageMasiv)
@@ -59,7 +74,8 @@ function App() {
         <div className='cont' key={index}>
           <img className='img' src={image[index].img} alt="image" />
          <a className='aTeg'  href={image[index].url}>{el.name}</a>
-         
+         {/* {console.log(image)} */}
+         {/* {console.log(products)} */}
         </div>
       ))} 
 
